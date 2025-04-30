1cf1682ba95c3d620b3450fe46600efd1c7db1b9
Gautam Tirkha <Gautam Tirkha>
2025-04-30 09:57:16 +0530

    Implement Firebase integration and add login/splash modules

---
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+import '../../../routes/app_pages.dart';
+
+class SplashController extends GetxController {
+  @override
+  void onInit() {
+    super.onInit();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      final User? currentUser = FirebaseAuth.instance.currentUser;
+      if (currentUser == null) {
+        Get.toNamed(Routes.LOGIN);
+      } else {
+        Get.toNamed(Routes.HOME);
+      }
+    });
+  }
+}
