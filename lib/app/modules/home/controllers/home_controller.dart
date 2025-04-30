7c1ef9b6b0cdc2768245dc6f326fce3cdc3d1f2a
Gautam Tirkha <Gautam Tirkha>
2025-04-30 13:31:12 +0530

    Refactor HomeController: Move navigation to login route from logout method to onInit for improved state management.

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -4,8 +4,17 @@ import 'package:get/get.dart';
 import '../../../routes/app_pages.dart';
 
 class HomeController extends GetxController {
+  @override
+  void onInit() {
+    FirebaseAuth.instance.authStateChanges().listen((event) {
+      if (event == null) {
+        Get.offAllNamed(Routes.LOGIN);
+      }
+    });
+    super.onInit();
+  }
+
   void logout() {
     FirebaseAuth.instance.signOut();
-    Get.offAllNamed(Routes.LOGIN);
   }
 }
