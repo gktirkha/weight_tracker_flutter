657a6638f5c0922a11fa82d6303b45d0c05508cd
gautam <gautam@gautam.com>
2025-05-04 07:41:12 +0530

    Refactor loading animation: Update loader styles and streamline loading logic in index.html and flutter_bootstrap.js for improved performance and maintainability.

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,23 @@
+{{flutter_js}}
+{{flutter_build_config}}
+
+
+
+const loadingDiv = document.createElement('div');
+loadingDiv.className = "loading";
+document.body.appendChild(loadingDiv);
+const loaderDiv = document.createElement('div');
+loaderDiv.className = "loader";
+loadingDiv.appendChild(loaderDiv);
+
+// Customize the app initialization process
+_flutter.loader.load({
+    onEntrypointLoaded: async function (engineInitializer) {
+        const appRunner = await engineInitializer.initializeEngine();
+        
+        if (document.body.contains(loadingDiv)) {
+            document.body.removeChild(loadingDiv);
+        }
+        await appRunner.runApp();
+    }
+});
